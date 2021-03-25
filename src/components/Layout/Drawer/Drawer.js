16fdbe543c81069f951b805d79e5f8d345346d13
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-03-25 14:18:53 +0600

    Move the logo folder to the UI folder

--- src/components/Layout/Drawer/Drawer.js
+++ src/components/Layout/Drawer/Drawer.js
@@ -1,25 +1,25 @@
 import Backdrop from "../../UI/Backdrop";
-import Logo from "../../Logo/Logo";
+import Logo from "../../UI/Logo/Logo";
 import Nav from "../../Nav/Nav";
 
 import classes from "./Drawer.module.css";
 
 const Drawer = ({ open, closeDrawer }) => {
-    const drawerClasses = [
-      classes.content,
-      open ? classes.open : classes.close
-    ];
-  
-    return (
-      <div className={classes.Drawer}>
-        <Backdrop show={open} click={closeDrawer} />
-        <div className={drawerClasses.join(" ")}>
-          <Logo />
-          <Nav />
-        </div>
+  const drawerClasses = [
+    classes.content,
+    open ? classes.open : classes.close
+  ];
+
+  return (
+    <div className={classes.Drawer}>
+      <Backdrop show={open} click={closeDrawer} />
+      <div className={drawerClasses.join(" ")}>
+        <Logo />
+        <Nav />
       </div>
-    );
-  }
-   
-  export default Drawer;
+    </div>
+  );
+}
+
+export default Drawer;
 
