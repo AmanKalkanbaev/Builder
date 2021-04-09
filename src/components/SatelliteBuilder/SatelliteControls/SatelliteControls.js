2be4a939181b9bcacedf33ed7cd71525a61621ca
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-04-09 13:52:48 +0600

    formated price and order from control

--- src/components/SatelliteBuilder/SatelliteControls/SatelliteControls.js
+++ src/components/SatelliteBuilder/SatelliteControls/SatelliteControls.js
@@ -3,7 +3,7 @@ import Button from "../../UI/Button/Button";
 
 import classes from "./SatelliteControls.module.css";
 
-const SatelliteControls = ({ satellites, addSatellite, removeSatellite, canBuy, setIsBuying }) => {
+const SatelliteControls = ({ satellites, addSatellite, removeSatellite, canBuy, setIsBuying, price}) => {
   const results = [];
   for (const satellite in satellites) {
     results.push(<SatelliteControl
@@ -17,7 +17,12 @@ const SatelliteControls = ({ satellites, addSatellite, removeSatellite, canBuy, 
     <div className={classes.SatelliteControls}>
       <strong>Satellites</strong>
       {results}
-      <Button onClick={() => setIsBuying(true)} disabled={!canBuy}>Order</Button>
+      <div className={classes.order}>
+        <div className={classes.price}>
+          Total price:  <span> $ {price.toFixed(0)} M </span>
+        </div>
+        <Button onClick={() => setIsBuying(true)} disabled={!canBuy}>Order</Button>
+      </div>
     </div>
   );
 }
