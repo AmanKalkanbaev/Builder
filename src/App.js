9340944f324957e1c46e8042b3af281f63ba33ec
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-04-16 15:54:15 +0600

    Comented starts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Layout from "./components/Layout/Layout";
 import SatelliteBuilder from "./components/SatelliteBuilder/SatelliteBuilder";
-import Stars from "./components/UI/Stars/Stars"
+// import Stars from "./components/UI/Stars/Stars"
 
 import "./App.css";
 
